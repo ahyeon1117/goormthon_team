4bc7d03cff6c57ea96a589ef2b26923e12e8e801
ahyeon1117 <ahyeon1117@naver.com>
2024-11-20 05:15:08 +0900

    [fix] - 네이밍 수정 - 시멘틱 수정

--- todo/instagram/src/App.js
+++ todo/instagram/src/App.js
@@ -1,27 +1,27 @@
-import Article from "./components/Article.js";
+import SuggestSidebar from "./components/SuggestSidebar.js";
 import Contents from "./components/Contents.js";
-import Sidebar from "./components/Sidebar.js";
+import NavSidebar from "./components/NavSidebar.js";
 import Story from "./components/Story.js";
 
 function App() {
   return (
     <div className="body">
-      <nav className="sidebar">
-        <Sidebar />
-      </nav>
-      <div className="main">
-        <main className="contents-area mgt16">
+      <aside className="sidebar">
+        <NavSidebar />
+      </aside>
+      <main className="main">
+        <article className="contents-area mgt16">
           <div className="story">
             <Story />
           </div>
           <div className="contents">
             <Contents />
           </div>
-        </main>
-        <article className="article">
-          <Article />
         </article>
-      </div>
+        <aside className="article">
+          <SuggestSidebar />
+        </aside>
+      </main>
     </div>
   );
 }
