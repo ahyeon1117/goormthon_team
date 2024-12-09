8f30d63bd9b5cc3e9a8df03474899979230d8bad
ahyeon1117 <ahyeon1117@naver.com>
2024-12-09 18:09:10 +0900

    [feat] 크기 조정 css 배경 제거

--- todo/src/components/Calendar.js
+++ todo/src/components/Calendar.js
@@ -1,11 +1,38 @@
 import "../assets/css/calendar.css";
+import ReactCalendar from "react-calendar";
+import { useState } from "react";
+
 function Calendar() {
+  const [selectedDate, setSelectedDate] = useState([new Date(), new Date()]);
+  const formatDate = (date) => {
+    if (Array.isArray(date)) {
+      const [startDate] = date;
+      return `${startDate.getFullYear()}.${String(
+        startDate.getMonth() + 1
+      ).padStart(2, "0")}`;
+    }
+    return `${date.getFullYear()}.${String(date.getMonth() + 1).padStart(
+      2,
+      "0"
+    )}`;
+  };
   return (
-    <section>
-      <h1>2024.12</h1>
-      <p>달력 렌더링</p>
+    <section className="calendar">
+      <h1>{formatDate(selectedDate) || ""}</h1>
+      <ReactCalendar
+        onChange={setSelectedDate}
+        value={selectedDate}
+        view="month"
+        showWeekNumbers={false}
+        showNavigation={false}
+        prev2Label={null}
+        next2Label={null}
+        formatDay={(locale, date) =>
+          date.toLocaleDateString("en", { day: "2-digit" })
+        }
+        showNeighboringMonth={false}
+      ></ReactCalendar>
     </section>
   );
 }
-
 export default Calendar;
