4939876d5f67468bf3987d91545983973bda8d01
Juhyun Lee <joo979797@naver.com>
2024-12-16 19:38:16 +0900

    [feat] 카테고리 조회 모달, 카테고리 모달 CSS 파일 추가

--- todo/src/components/modals/CategoryModals.js
+++ todo/src/components/modals/CategoryModals.js
@@ -1,11 +1,16 @@
 import React, { useContext } from 'react'
 import { CategoryContext } from '../../context/CategoryContext';
+import CategoryViewModal from './CategoryViewModal';
 
 export default function CategoryModals() {
   const {categoryModals} = useContext(CategoryContext);
 
   return (
     <>
+      {/* 카테고리 조회 모달 */}
+      {categoryModals.isViewOpen && (
+        <CategoryViewModal />
+      )}
     </>
   )
 }
