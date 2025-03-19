23e97f7ec9e7838083716059e748c379aaaea7de
Jung HaEun <codingha00@gmail.com>
2025-03-19 17:41:40 +0900

    [feat] Main 출력

--- commerce/frontend/src/App.tsx
+++ commerce/frontend/src/App.tsx
@@ -1,17 +1,21 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
-import Header from './layout/Header/Header'
-import FixedHeader from './layout/Header/FixedHeader'
+import GlobalFontStyle from './styles/Global/GlobalFont.styled';
+import {GlobalPageSize} from './styles/Global/GlobalPage.styled';
+import Main from './pages/MainPage/components/Main/Main.tsx';  // Main 컴포넌트 import
 
 function App() {
-  return (
-    <div className="App">
-      {/* 여기에 공통 레이아웃 요소를 추가할 수 있습니다 (헤더, 푸터 등) */}
-      <Header />
-      <Outlet />
-      <FixedHeader />
-    </div>
-  )
+    return (
+        <div className="App">
+            {/* 여기에 공통 레이아웃 요소를 추가할 수 있습니다 (헤더, 푸터 등) */}
+            <GlobalFontStyle />
+            <GlobalPageSize />
+
+            <Main />
+            <Outlet />
+        </div>
+    )
 }
 
 export default App
+
