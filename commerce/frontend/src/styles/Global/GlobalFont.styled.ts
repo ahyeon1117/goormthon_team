714d12f2d348a86234a2a758c17689cc48e651ce
Kim Keon-Young <diamondgonny@gmail.com>
2025-03-21 11:30:01 +0900

    [fix] 폰트 파일 경로 재작성 및 직접 import로 변경
    - 상세페이지 작업 중 폰트 적용이 잘 되지 않는걸 발견함
    - 기존의 폰트 파일 경로를 직접 import 방식으로 변경함
    - 추가로 각 폰트에 대해 font-display 속성을 추가하여 로딩중에 커스텀 폰트로 전환하게 함

--- commerce/frontend/src/styles/Global/GlobalFont.styled.ts
+++ commerce/frontend/src/styles/Global/GlobalFont.styled.ts
@@ -1,35 +1,37 @@
 // src/styles/globalStyles.ts
 import { createGlobalStyle } from 'styled-components';
-//폰트 사용
-// 폰트 파일 경로 상단에서 정의
-const LIGHT_FONT_PATH = 'src/assets/fonts/GmarketSansTTFLight.ttf';
-const REGULAR_FONT_PATH = 'src/assets/fonts/GmarketSansTTFMedium.ttf';
-const BOLD_FONT_PATH = 'src/assets/fonts/GmarketSansTTFBold.ttf';
+// 폰트 직접 import
+import LightFont from '../../assets/fonts/GmarketSansTTFLight.ttf';
+import RegularFont from '../../assets/fonts/GmarketSansTTFMedium.ttf';
+import BoldFont from '../../assets/fonts/GmarketSansTTFBold.ttf';
 
 const GlobalFontStyle = createGlobalStyle`
   @font-face {
     font-family: "GmarketSans";
     font-weight: 300;
-    src: url(${LIGHT_FONT_PATH}) format("truetype");
+    src: url(${LightFont}) format("truetype");
+    font-display: swap;
   }
 
   @font-face {
     font-family: "GmarketSans";
     font-weight: 400;
-    src: url(${REGULAR_FONT_PATH}) format("truetype");
+    src: url(${RegularFont}) format("truetype");
+    font-display: swap;
   }
 
   @font-face {
     font-family: "GmarketSans";
     font-weight: 700;
-    src: url(${BOLD_FONT_PATH}) format("truetype");
+    src: url(${BoldFont}) format("truetype");
+    font-display: swap;
   }
 
   * {
       font-family: "GmarketSans", sans-serif;
       font-weight: 400; /* 기본 굵기: Regular */
   }
-  
+
 `;
 
 export default GlobalFontStyle;
