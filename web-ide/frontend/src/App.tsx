7cc1c55081275755ab3f227916ad58bac6bd8560
Lumire-ee <58719969+Lumire-ee@users.noreply.github.com>
2025-04-11 19:42:56 +0900

    [refactor] 페이지 파일 디렉토리 구조 정리 및 import 경로 수정
    - 기존 페이지 파일들의 src/pages 이동에 따른 App.tsx import 경로 수정

--- web-ide/frontend/src/App.tsx
+++ web-ide/frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
-import SignupPage from './SignupPage';
-import LoginPage from './LoginPage';
-import MainPage from './MainPage';
-import DashboardPage from './DashboardPage';
-import EditorWorkspace from './components/EditorWorkspace';
+import SignupPage from './pages/SignupPage';
+import LoginPage from './pages/LoginPage';
+import MainPage from './pages/MainPage';
+import DashboardPage from './pages/DashboardPage';
+import Workspace from './pages/WorkspacePage';
 
 function App() {
   return (
@@ -12,11 +12,12 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/workspace" element={<EditorWorkspace />} />
         <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/workspace" element={<Workspace />} />
       </Routes>
     </div>
   );
 }
 
 export default App;
+
