be7a7d361cf758bdfd3ca7e76b80102516ff4648
hyeji <wedgejjangdol@gmail.com>
2025-04-11 16:42:35 +0900

    [feat] ChatRoom/File/Folder/Message Repository 추가

---
+++ web-ide/backend/src/main/java/io/goorm/backend/repository/MessageRepository.java
@@ -0,0 +1,9 @@
+package io.goorm.backend.repository;
+
+import io.goorm.backend.entity.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Long> {
+}
