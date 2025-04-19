d6c6a09f324a2f15c8e3890c9aadd7a749220610
Juhyun Lee <joo979797@naver.com>
2025-04-20 01:37:59 +0900

    [fix] 삭제된 파일 되돌리기

--- web-ide/backend/src/main/java/io/goorm/backend/repository/FileRepository.java
+++ web-ide/backend/src/main/java/io/goorm/backend/repository/FileRepository.java
@@ -1,7 +1,18 @@
 package io.goorm.backend.repository;
 
 import io.goorm.backend.entity.File;
+import io.goorm.backend.entity.Folder;
+import io.goorm.backend.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FileRepository extends JpaRepository<File, Long> {
+    Optional<File> findByNameAndFolder(String fileName, Folder folder);
+    Optional<File> findByNameAndProjectAndFolderIsNull(String fileName, Project project);
+
+    void deleteByFolder(Folder folder);
+    void deleteAllByFolder(Folder folder);
+
+
 }
--- web-ide/backend/src/main/java/io/goorm/backend/service/UserService.java
+++ web-ide/backend/src/main/java/io/goorm/backend/service/UserService.java
@@ -30,4 +30,22 @@ public class UserService implements UserDetailsService {
 
         return CustomUserDetails.of(user);
     }
+
+    //JWT에서 받아온 id로 User 조회
+    public User findById(String id) {
+        Long userId = Long.parseLong(id);
+        return userRepository.findById(userId) //id로 사용자 조회
+                .orElseThrow(() -> new RuntimeException("User not found."));
+    }
+
+    //사용자 생성
+    public User createUser(String username, String email, String password) {
+        User user = User.builder()
+                .username(username)
+                .email(email)
+                .password(password)
+                .build();
+        return userRepository.save(user);
+    }
+
 }
