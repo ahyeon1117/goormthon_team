391d1865fd17cacdbb562bc766859d4f94d85649
Juhyun Lee <joo979797@naver.com>
2025-04-20 23:56:16 +0900

    [feat] ApiResponse에 커스텀 메시지 응답 메서드 추가

--- web-ide/backend/src/main/java/io/goorm/backend/global/response/ApiResponse.java
+++ web-ide/backend/src/main/java/io/goorm/backend/global/response/ApiResponse.java
@@ -13,10 +13,20 @@ public class ApiResponse<T> {
     private final String message; // 응답 메시지
     private final T data; // 응답 데이터
 
+    // 단순 데이터 응답에 사용
     public static <T> ApiResponse<T> success(T data) {
         return ApiResponse.<T>builder()
                 .code(200)
                 .message("success")
+                .data(data)
+                .build();
+    }
+
+    // 메시지를 추가하여 성공 응답 반환
+    public static <T> ApiResponse<T> success(T data, String message) {
+        return ApiResponse.<T>builder()
+                .code(200)
+                .message(message)
                 .data(data)
                 .build();
     }
