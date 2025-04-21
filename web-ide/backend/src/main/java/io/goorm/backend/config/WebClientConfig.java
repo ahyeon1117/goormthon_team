b7deae411d597ccf5e6ddb316ef338ce08837b97
Jung HaEun <codingha00@gmail.com>
2025-04-21 15:27:04 +0900

    [feat] Bean 추가

--- web-ide/backend/src/main/java/io/goorm/backend/config/WebClientConfig.java
+++ web-ide/backend/src/main/java/io/goorm/backend/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package io.goorm.backend.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -12,8 +13,13 @@ public class WebClientConfig {
     @Bean
     public WebClient webClient() {
         return WebClient.builder()
-                .baseUrl("http://localhost:8000") // FastAPI 서버 주소
+                .baseUrl("http://localhost:8000") // FastAPI 서버 주소 수정
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
     }
 }
