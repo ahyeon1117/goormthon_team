f7788b8b381143dd4d978a9ed28646f191a27a24
Juhyun Lee <joo979797@naver.com>
2025-04-22 04:55:19 +0900

    [feat] UserInit - 임시 초기 사용자 2명 등록

--- web-ide/backend/src/main/java/io/goorm/backend/init/UserInit.java
+++ web-ide/backend/src/main/java/io/goorm/backend/init/UserInit.java
@@ -18,27 +18,39 @@ public class UserInit {
     private final PasswordEncoder passwordEncoder;
 
     /**
-     * 개발용 초기 사용자 생성
+     * 개발용 초기 사용자 2명 생성
      *
      * username: 김구름
      * email: test@gmail.com
+     * password: test1234!
+     * 
+     * username: 노구름
+     * email: goorm@gmail.com
      * password: test1234!
      */
     @PostConstruct
     public void init() {
 
-        // 사용자가 존재하지 않는다면
+        // 사용자가 존재하지 않는다면 (초기화 중복 방지)
         if (userRepository.count() == 0) {
             // 초기 사용자 생성
-            User user = User.builder()
-                    .username("김구름")
+            User user1 = User.builder()
+                    .username("정구름")
                     .email("test@gmail.com")
+                    .password(passwordEncoder.encode("test1234!"))
+                    .build();
+            
+            User user2 = User.builder()
+                    .username("노구름")
+                    .email("goorm@gmail.com")
                     .password(passwordEncoder.encode("test1234!"))
                     .build();
 
             // 사용자 저장
-            userRepository.save(user);
-            log.info("[USER_INIT] 개발용 초기 사용자 생성 완료 - 이메일: {}", user.getEmail());
+            userRepository.save(user1);
+            userRepository.save(user2);
+
+            log.info("[USER_INIT] 개발용 초기 사용자 2명 생성 완료 - 이메일: {}, {}", user1.getEmail(), user2.getEmail());
         }
     }
 }
