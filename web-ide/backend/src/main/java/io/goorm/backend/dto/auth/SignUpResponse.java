90216c21ff443ddf94f78d43e5add92cfdfcbcd4
Juhyun Lee <joo979797@naver.com>
2025-04-22 18:14:45 +0900

    [refactor] 회원가입 시 DTO(SignUpResponse)로 반환, 로그인 시 username도 응답하도록 변경

--- web-ide/backend/src/main/java/io/goorm/backend/dto/auth/SignUpResponse.java
+++ web-ide/backend/src/main/java/io/goorm/backend/dto/auth/SignUpResponse.java
@@ -1,11 +1,14 @@
 package io.goorm.backend.dto.auth;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class LoginResponse {
-    private final String accessToken;
-    private final String username;
+public class SignUpResponse {
+
+    private Long id;
+    private String username;
+    private String email;
 }
