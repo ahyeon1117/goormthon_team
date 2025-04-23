3cc55e7cad26f16f90aff575288215434765a37d
Jung HaEun <codingha00@gmail.com>
2025-04-24 03:28:29 +0900

    [chore] 빌드 테스트 임시 정지

--- web-ide/backend/run.sh
+++ web-ide/backend/run.sh
@@ -2,7 +2,13 @@
 
 echo "🔨 Gradle 빌드 시작..."
 
-./gradlew build || { echo "❌ 빌드 실패"; exit 1; }
+#./gradlew build || { echo "❌ 빌드 실패"; exit 1; }
+if ./gradlew build -x test; then
+    echo "✅ Gradle 빌드 성공"
+else
+    echo "❌ Gradle 빌드 실패"
+    exit 1
+fi
 
 echo "🐳 Docker Compose 실행..."
 docker-compose up --build -d
