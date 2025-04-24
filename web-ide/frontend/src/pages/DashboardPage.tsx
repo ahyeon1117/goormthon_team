52f7935cd73edc771726f4d34b22ae507e310e92
Lumire-ee <58719969+Lumire-ee@users.noreply.github.com>
2025-04-24 17:56:32 +0900

    [fix] 프로젝트 ID 참조 오류 수정 - project.id -> project.projectId

--- web-ide/frontend/src/pages/DashboardPage.tsx
+++ web-ide/frontend/src/pages/DashboardPage.tsx
@@ -27,7 +27,7 @@ const DashboardContent = () => {
       </div>
       <div className="p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
         {context.projects.map((project) => (
-          <ContainerCard key={project.id} project={project} />
+          <ContainerCard key={project.projectId} project={project} />
         ))}
       </div>
     </div>
